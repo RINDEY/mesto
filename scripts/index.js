f0c1a2857e2c8d8c6c1fb767e1e603b31f935335
Rita Eremina <rindey8@gmail.com>
2023-03-06 01:21:26 +0300

    feat: добавила карточки с помощью JS

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,50 @@ closeButton.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+const elements = [
+  {
+    name: 'Карачаевск',
+    alt: 'Древний храм в горах',
+    link: './images/karachaevsk.png'
+  },
+  {
+    name: 'Гора Эльбрус',
+    alt: 'Заснеженный Эльбрус',
+    link: './images/elbrus.png'
+  },
+  {
+    name: 'Домбай',
+    alt: 'Равнины в закате',
+    link: './images/dombay.png'
+  },
+  {
+    name: 'Сахалин',
+    alt: 'Побережье Охотского моря',
+    link: './images/sakhalin.jpg'
+  },
+  {
+    name: 'Курилы',
+    alt: 'Спящий вулкан',
+    link: './images/kurily.jpg'
+  },
+  {
+    name: 'Карелия',
+    alt: 'Горный парк Рускеала',
+    link: './images/karelia.jpg'
+  },
+];
+
+const page = document.querySelector('.page');
+const elementsBox = document.querySelector('.elements');
+
+const createElement = (element) => {
+  const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
+  const elementHeading = newElement.querySelector('.element__description');
+  elementHeading.textContent = element.name;
+  const elementImage = newElement.querySelector('.element__photo');
+  elementImage.setAttribute('src', element.link);
+  elementImage.setAttribute('alt', element.alt);
+  elementsBox.prepend(newElement);
+}
+
+elements.forEach(createElement);
